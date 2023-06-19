4416f96c9009724ef20f10dcbf75554a28e2d6f2
kaushal1999 <kaushalwaghela1999@gmail.com>
2023-06-19 17:49:17 +0530

    made editors resizable

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,7 +1,45 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Controlled as CodeMirror } from "react-codemirror2";
+import { Button } from "@mui/material";
+import CloseFullscreenIcon from "@mui/icons-material/CloseFullscreen";
+import "codemirror/lib/codemirror.css";
+import { Resizable } from "re-resizable";
+import OpenInFullIcon from "@mui/icons-material/OpenInFull";
 
 export default function Editor() {
+  const [width, setWidth] = useState(320);
+  const [height, setHeight] = useState(200);
+  
   return (
-    <div>Editor</div>
-  )
+    <div className="container">
+      <Resizable
+        size={{ width: width, height: height }}
+        onResizeStop={(e, direction, ref, d) => {
+          setHeight(height + d.height);
+          setWidth(width + d.width);
+        }}
+      >
+        <div>
+          <Button
+            variant="outlined"
+            onClick={() => {
+              if (width === 20) setWidth(320);
+              else setWidth(20);
+            }}
+          >
+            {(width===20)? <OpenInFullIcon />
+            :<CloseFullscreenIcon/>}
+          </Button>
+        </div>
+        <CodeMirror
+          value="kasuhal"
+          // options={options}
+          onBeforeChange={(editor, data, value) => {
+            this.setState({ value });
+          }}
+          onChange={(editor, data, value) => {}}
+        />
+      </Resizable>
+    </div>
+  );
 }
