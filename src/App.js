a37281bab6623597f1928c7d060536f9729fca68
kaushal1999 <kaushalwaghela1999@gmail.com>
2023-07-27 12:37:28 +0530

    changes for deployment

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ function App() {
   return (
     <div>
       <div className="header">
-        <CodeIcon></CodeIcon>
+        {/* <CodeIcon></CodeIcon> */}
         <Button
           onClick={() => {
             localStorage.clear();
